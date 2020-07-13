4c788419c5d0fca95d5ba3578ef6d0a0cb0f7a66
jgam <jgam@alumni.nd.edu>
2020-07-13 19:53:53 +0900

    [db] schema done

---
+++ entity/user.entity.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+    {   
+        username: {
+            type: String,
+            required: [true, 'username is required'],
+        },
+        email:{
+            type: String,
+            required: true,
+            trim:true,
+        },
+        password:{
+            type:String,
+            required: true,
+            trim: true,
+        }
+    },
+    {timestamps: true
+    }
+)
+
+module.exports = new mongoose.model("User", userSchema);
--- index.js
+++ index.js
@@ -2,6 +2,36 @@ const express = require('express');
 
 const app = express()
 
+app.get('/', (req,res,next) => {
+    res.status(200).json({
+        msg: '200 success!',
+    })
+})
+
+//register
+app.post('/api/user/register', (req,res,next) => {
+    //get userinfo
+    
+    res.status(201).json({
+        result: 'succesfully registered new user'
+    })
+})
+
+//login
+app.post('/api/user/login', (req,res,next) => {
+    res.status(201).json({
+        result: 'succesfully Logged in'
+    })
+})
+
+//get my profile
+app.get('/api/user/me', (req,res,next) => {
+    res.status(201).json({
+        result: 'this is my information'
+    })
+})
+
+
 const PORT = 3333;
 
 app.listen(PORT, () => {console.log(`running at current port ${PORT} boi!`)})
